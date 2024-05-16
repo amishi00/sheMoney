e3725608914c840a880086a188ed027be152487e
Amishi <ag2424@cornell.edu>
2024-05-16 16:09:24 -0400

    Initial Commit

---
+++ SheMoney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SheMoney
+//
+//  Created by Ashish Kumar on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
